86a58cdad3a53d8e63ee3f45559357396238d0ce
Anthony Tobias <aelias2001@msn.com>
2026-02-09 17:17:06 -0800

    adding repo-name to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://anttobias2.github.io',
+  site: 'https://anttobias2.github.io/move-ready-concierge/',
   base: '/move-ready-concierge/', // <-- IMPORTANT
 });
