2db5547b4906535bbbf512f828d47b3bf99f3894
Anthony Tobias <aelias2001@msn.com>
2026-02-09 16:59:28 -0800

    Testing fix for deployment issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://anttobias2.github.io',
-  base: '/move-ready-concierge', // <-- IMPORTANT
+  site: 'https://anttobias2.github.io/repo-name',
+  base: '/move-ready-concierge/', // <-- IMPORTANT
 });
